6c74ba4ab51b6a95852725ce9f36bca6f0c29e9c
Seohyun Lee <studioseorii@gmail.com>
2023-08-28 19:50:56 +0900

    fix: fix importing

--- src/loq2/lib.py
+++ src/loq2/lib.py
@@ -4,7 +4,11 @@ import os
 
 so = glob.glob(os.path.abspath(os.path.join(__file__, '../../**/*.so')), recursive=True)
 if len(so) == 0:
+    so = glob.glob(os.path.abspath(os.path.join(__file__, '../../**/*.lib')), recursive=True)
+if len(so) == 0:
     so = glob.glob(os.path.abspath(os.path.join(__file__, '../../**/*.dll')), recursive=True)
+if len(so) == 0:
+    so = glob.glob(os.path.abspath(os.path.join(__file__, '../../**/*.dylib')), recursive=True)
 if len(so) == 0:
     raise ImportError("Cannot import native library")
 
